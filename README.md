9b958bcfd6139fd6c15355befe7ea6a7daf6f795
Mike Arvela <mike.arvela@futurice.com>
2015-08-16 12:03:33 +0300

    Make globally installable

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# jenkinson
+# jestas
 
 Show Jenkins build statuses from the command line.
+
+![Screenshot](https://github.com/mieky/jestas/raw/master/screenshot.png)
+
+Requires node 0.10+.
+
+Installation:
+`npm install -g jestas`
+
+## Usage:
+
+`jestas [search]`
+
+You can provide an optional fuzzy search term to filter the results.
+
+Uses the NodeJS QA repository configured in `./jestas.json` by default.
+
+## Configuration
+
+Put configuration file `jestas.json` to either the current directory or one of its parents, where findConfig will pick it up. It should look like this:
+
+```
+{
+    "node": "http://jenkins.nodejs.org/"
+}
+```
+
+Where `http://jenkins.nodejs.org/` is the root URL of the Jenkins installation you want to query.
--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var findConfig = require("find-config");
 var fetch = require("node-fetch");
+var fuzzy = require("fuzzy");
 var util = require("util");
 var clc = require("cli-color");
 var pad = require("pad");
 var _ = require("lodash");
 
-var configFile = findConfig("jenkinson.json");
+var configFile = findConfig("jestas.json");
 
 if (!configFile) {
-    console.log("Configuration file jenkinson.json not found.");
+    console.log(`Configuration file not found. Create a 'jestas.json' that looks like this:
+{
+    \"node\": \"http://jenkins.nodejs.org/\"
+}
+`);
     process.exit(1); // eslint-disable-line
 }
 
 var config = require(configFile);
 console.log("Using configuration", config);
+var filterStr = process.argv.splice(2).join("");
 
 function colorToStatus(color) {
     if (color === "blue") {
@@ -22,22 +28,23 @@ function colorToStatus(color) {
     return clc.red("fail");
 }
 
-function filterByName(jobs) {
-    // return jobs.filter(j => { true });
-    return jobs;
-}
+var filterByName = function(str, arr) {
+    var includedNames = fuzzy.filter(str, arr.map(i => i.name))
+        .map(e => e.string);
+    return arr.filter(i => includedNames.indexOf(i.name) >= 0);
+}.bind(null, filterStr);
 
 function mapColorsToStatuses(jobs) {
-    return jobs.map(job => {
-        return {
-            "status": colorToStatus(job.color),
-            "name": job.name
-        };
-    });
+    return jobs.map(job => ({
+        "status": colorToStatus(job.color),
+        "name": job.name
+    }));
 }
 
 function prettyPrint(job) {
-    var jobStatus = util.format("%s %s", pad(job.status, 5, { colors: true }), job.name);
+    var jobStatus = util.format("%s %s",
+        pad(job.status, 5, { colors: true }),
+        job.name);
     console.log(jobStatus);
 }
 
@@ -45,7 +52,7 @@ var prefix = _.first(_.keys(config));
 var url = config[prefix] + "/api/json?pretty=true";
 fetch(url)
     .then(r => r.json())
-    .then(json => { return json.jobs; })
+    .then(json => json.jobs)
     .then(filterByName)
     .then(mapColorsToStatuses)
     .then(jobs => {
